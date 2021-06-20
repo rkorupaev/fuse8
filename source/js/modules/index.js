48b17c27648f6e5bb03752d751ee8e210e512a73
Roman Korupaev <rkorupaev@gmail.com>
2021-06-20 21:41:57 +0300

    filter works

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -2,6 +2,7 @@ const DOWNLOAD_URL = "https://603e38c548171b0017b2ecf7.mockapi.io/homes";
 const STATUS_CODE_OK = "200";
 const INDEPENDENT_LIVING = "Independent living";
 const SUPPORT_AVALIABLE = "Restaurant & Support available";
+const FILTER_INPUT_CLASS = ".filter-block__input";
 
 const fetchData = (onLoad, onError) => {
   const xhr = new XMLHttpRequest();
@@ -30,22 +31,38 @@ const fetchData = (onLoad, onError) => {
 }
 
 fetchData((data) => {
-  if (data) {
-    mapCards(data);
-
+    if (data) {
+      let filteredData = data;
+      const cardBlock = document.querySelector(".main-body__aparts-list");
+      mapCards(filteredData, cardBlock);
+      const filterInput = document.querySelector(FILTER_INPUT_CLASS);
+      filterInput.addEventListener("change", (evt) => {
+          if (evt.currentTarget.value.length > 3) {
+            filteredData = filterData(data, evt);
+            mapCards(filteredData, cardBlock);
+          } else if (evt.currentTarget.value.length === 0) {
+            mapCards(data, cardBlock);
+          }
+        }
+      );
+    }
+  },
+  (status) => {
+    console.log(status);
   }
-}, (status) => {
-  console.log(status);
-});
+)
+;
 
-const mapCards = (data) => {
-  const cardBlock = document.querySelector(".main-body__aparts-list");
+const mapCards = (data, block) => {
+  block.textContent = "";
 
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < 6; i++) {
     const card = document.createElement("li");
     card.classList.add("aparts-list__item");
     card.append(document.getElementById("card").content.cloneNode(true));
 
+    let imageIndex = getRandomInt(1, 3);
+    card.querySelector(".card__image").setAttribute("src", "./img/apart" + imageIndex + ".png");
     switch (data[i].type) {
       case "IndependentLiving" :
         card.querySelector(".card__type").textContent = INDEPENDENT_LIVING;
@@ -58,6 +75,16 @@ const mapCards = (data) => {
     card.querySelector(".card__title").textContent = data[i].title;
     card.querySelector(".card__address").textContent = data[i].address;
     card.querySelector(".card__price").textContent = "£" + data[i].price;
-    cardBlock.append(card);
+    block.append(card);
   }
+};
+
+const filterData = (data, evt) => {
+  let filteredData = data.filter(appart => appart.title.toLowerCase().includes(evt.currentTarget.value.toLowerCase()));
+  return filteredData;
+};
+
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
